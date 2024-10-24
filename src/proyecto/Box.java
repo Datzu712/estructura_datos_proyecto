5211c0ffa5b5e94a205b7de9b0624bf262c90ff8
Datzu712 <devdatzucl@gmail.com>
2024-10-24 15:32:19 -0600

    queue implemented for box class

--- src/proyecto/Box.java
+++ src/proyecto/Box.java
@@ -2,7 +2,6 @@ package proyecto;
 
 import java.io.Serializable;
 
-// clase cola
 public class Box implements Serializable {
     private static int nextId = 0;
 
@@ -13,7 +12,6 @@ public class Box implements Serializable {
     public String type;
     public boolean preferential;
 
-    //private ArrayList<Client> queue = new ArrayList<Client>();
 
     public Box(String type, boolean preferential) {
         this.id = nextId++;
@@ -53,29 +51,32 @@ public class Box implements Serializable {
         return ticket;
     }
 
-    // public void enqueue(Client client) {
-    //     queue.add(client);
-    // }
+    public void enqueue(Ticket ticket) {
+        if (currentTicket == null) {
+            currentTicket = ticket;
+        } else {
+            Ticket lastTicket = currentTicket;
+            while (lastTicket.getNextTicket() != null) {
+                lastTicket = lastTicket.getNextTicket();
+            }
+            lastTicket.setNextTicket(ticket);
+        }
+        size++;
+    }
 
-    // public Client dequeue() {
-    //     if (queue.isEmpty()) {
-    //         return null;
-    //     }
-    //     return queue.remove(0);
-    // }
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
-    // public Client peek() {
-    //     if (queue.isEmpty()) {
-    //         return null;
-    //     }
-    //     return queue.get(0);
-    // }
+    @Override
+    public String toString() {
+        String output = "";
+        Ticket ticket = currentTicket;
+        while (ticket != null) {
+            output += ticket.toString() + "\n";
+            ticket = ticket.getNextTicket();
+        }
 
-    // public boolean isEmpty() {
-    //     return queue.isEmpty();
-    // }
-
-    // public int size() {
-    //     return queue.size();
-    // }
+        return output;
+    }
 }
